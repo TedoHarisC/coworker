ce4a29dae0d7a06a61a1a28c47d8e2f9055cb2ef
Tedo Haris Chandra <tedoharischandra29@gmail.com>
2024-09-21 16:08:48 +0700

    Membuat Halaman Dashboard

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,5 +1,4 @@
 import 'package:coworker/controllers/dashboard_controller.dart';
-import 'package:d_view/d_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -23,7 +22,35 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: DView.appBarCenter('Dashboard'),
+      body: Obx(() => dashboardController.currentFragment),
+      bottomNavigationBar: Obx(() {
+        return BottomNavigationBar(
+          backgroundColor: Colors.white,
+          currentIndex: dashboardController.index,
+          type: BottomNavigationBarType.fixed,
+          onTap: (value) {
+            dashboardController.index = value;
+          },
+          selectedFontSize: 14,
+          unselectedFontSize: 12,
+          selectedItemColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.grey,
+          selectedLabelStyle: const TextStyle(
+            fontWeight: FontWeight.w700,
+            height: 2,
+          ),
+          unselectedLabelStyle: const TextStyle(
+            fontWeight: FontWeight.w700,
+            height: 2,
+          ),
+          items: dashboardController.menu.map((e) {
+            return BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage(e['icon_off'])),
+              label: e['label'],
+            );
+          }).toList(),
+        );
+      }),
     );
   }
 }
