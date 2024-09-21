23722984afa8f82b60be38c702883c1e5b626f95
Tedo Haris Chandra <tedoharischandra29@gmail.com>
2024-09-21 15:56:08 +0700

    Menambahkan Controller untuk Dashboard

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,8 +1,24 @@
+import 'package:coworker/controllers/dashboard_controller.dart';
 import 'package:d_view/d_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  final DashboardController dashboardController =
+      Get.put(DashboardController());
+
+  @override
+  void dispose() {
+    dashboardController.clear();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
