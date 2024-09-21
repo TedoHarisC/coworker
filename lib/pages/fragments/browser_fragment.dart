d13dfbf250a40931e3feea67e410996f29b4b52a
Tedo Haris Chandra <tedoharischandra29@gmail.com>
2024-09-21 16:48:41 +0700

    Adding Browser Controller

--- lib/pages/fragments/browser_fragment.dart
+++ lib/pages/fragments/browser_fragment.dart
@@ -1,7 +1,22 @@
+import 'package:coworker/controllers/fragments/browser_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class BrowserFragment extends StatelessWidget {
+class BrowserFragment extends StatefulWidget {
   const BrowserFragment({super.key});
+
+  @override
+  State<BrowserFragment> createState() => _BrowserFragmentState();
+}
+
+class _BrowserFragmentState extends State<BrowserFragment> {
+  final browseController = Get.put(BrowserController());
+
+  @override
+  void dispose() {
+    browseController.clear();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
