b8edd1b84c61f71f094d0b0ac4faa3e2fbb2fb93
Tedo Haris Chandra <tedoharischandra29@gmail.com>
2024-09-23 16:44:19 +0700

    Membuat Halaman Dashboard sampai list categories

--- lib/pages/fragments/browser_fragment.dart
+++ lib/pages/fragments/browser_fragment.dart
@@ -1,4 +1,7 @@
 import 'package:coworker/controllers/fragments/browser_controller.dart';
+import 'package:coworker/controllers/user_controller.dart';
+import 'package:d_view/d_view.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,6 +14,7 @@ class BrowserFragment extends StatefulWidget {
 
 class _BrowserFragmentState extends State<BrowserFragment> {
   final browseController = Get.put(BrowserController());
+  final userController = Get.put(UserController());
 
   @override
   void dispose() {
@@ -20,6 +24,161 @@ class _BrowserFragmentState extends State<BrowserFragment> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView();
+    return ListView(
+      padding: const EdgeInsets.all(0),
+      children: [
+        SizedBox(
+          height: 414,
+          child: Stack(
+            children: [
+              Image.asset(
+                'assets/bg_discover_page.png',
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  header(),
+                  DView.height(30),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      'Anda butuh pekerja\napa untuk hari ini ?',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  DView.height(30),
+                  categories(),
+                  DView.height(10),
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget categories() {
+    return SizedBox(
+      height: 120,
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: browseController.categories.length,
+        itemBuilder: (context, index) {
+          Map category = browseController.categories[index];
+          return GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsets.only(
+                left: index == 0 ? 20 : 8,
+                right: index == browseController.categories.length - 1 ? 20 : 8,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              width: 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    category['icon'],
+                    width: 46,
+                    height: 46,
+                  ),
+                  DView.height(8),
+                  Text(
+                    category['label'],
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget header() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: const Color(0xffBFA8FF),
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.white,
+                width: 2,
+              ),
+            ),
+            alignment: Alignment.center,
+            child: Obx(() {
+              return Text(
+                userController.data.name?[0] ?? '',
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black,
+                ),
+              );
+            }),
+          ),
+          DView.width(8),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Obx(() {
+                  return Text(
+                    'Hi ${userController.data.name ?? ''}',
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black,
+                    ),
+                  );
+                }),
+                const Text(
+                  'Recruiter',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                )
+              ],
+            ),
+          ),
+          IconButton.filled(
+            onPressed: () {},
+            icon: const Badge(
+              smallSize: 10,
+              backgroundColor: Colors.red,
+              child: ImageIcon(
+                AssetImage('assets/ic_notification.png'),
+              ),
+            ),
+            style: const ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(Colors.white),
+              foregroundColor: WidgetStatePropertyAll(Colors.black),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
